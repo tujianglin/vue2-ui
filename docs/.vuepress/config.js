13cfae442e3bdd438de7d6a7af0b2208cd699def
涂江林 <tujianglin@MacBook-Pro.local>
2023-02-02 14:18:36 +0800

    feat: dist

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,6 +4,7 @@ const sidebar = require('./configs/sidebar');
 module.exports = {
   title: '9.3组件文档库',
   description: '基于Element-ui二次封装基础组件示例',
+  base: '/vue2-ui/',
   themeConfig: {
     repoLabel: 'GitHub',
     nav,
